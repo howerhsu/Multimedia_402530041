50028a7cb9592a4297f23c8b3ca16337266a2eac
howerhsu <hower910119@gmail.com>
2016-04-08 20:46:02 +0800

    no message

---
+++ calculator (1)/calculator/calculator/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtbox = new System.Windows.Forms.TextBox();
+            this.bt7 = new System.Windows.Forms.Button();
+            this.bt8 = new System.Windows.Forms.Button();
+            this.bt9 = new System.Windows.Forms.Button();
+            this.bt4 = new System.Windows.Forms.Button();
+            this.bt5 = new System.Windows.Forms.Button();
+            this.bt6 = new System.Windows.Forms.Button();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.bt3 = new System.Windows.Forms.Button();
+            this.bt0 = new System.Windows.Forms.Button();
+            this.btClear = new System.Windows.Forms.Button();
+            this.btPlus = new System.Windows.Forms.Button();
+            this.btMinus = new System.Windows.Forms.Button();
+            this.btAns = new System.Windows.Forms.Button();
+            this.btDiv = new System.Windows.Forms.Button();
+            this.btMulti = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtbox
+            // 
+            resources.ApplyResources(this.txtbox, "txtbox");
+            this.txtbox.Name = "txtbox";
+            this.txtbox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // bt7
+            // 
+            resources.ApplyResources(this.bt7, "bt7");
+            this.bt7.Name = "bt7";
+            this.bt7.UseVisualStyleBackColor = true;
+            this.bt7.Click += new System.EventHandler(this.bt7_Click);
+            // 
+            // bt8
+            // 
+            resources.ApplyResources(this.bt8, "bt8");
+            this.bt8.Name = "bt8";
+            this.bt8.UseVisualStyleBackColor = true;
+            this.bt8.Click += new System.EventHandler(this.bt8_Click);
+            // 
+            // bt9
+            // 
+            resources.ApplyResources(this.bt9, "bt9");
+            this.bt9.Name = "bt9";
+            this.bt9.UseVisualStyleBackColor = true;
+            this.bt9.Click += new System.EventHandler(this.bt9_Click);
+            // 
+            // bt4
+            // 
+            resources.ApplyResources(this.bt4, "bt4");
+            this.bt4.Name = "bt4";
+            this.bt4.UseVisualStyleBackColor = true;
+            this.bt4.Click += new System.EventHandler(this.bt4_Click);
+            // 
+            // bt5
+            // 
+            resources.ApplyResources(this.bt5, "bt5");
+            this.bt5.Name = "bt5";
+            this.bt5.UseVisualStyleBackColor = true;
+            this.bt5.Click += new System.EventHandler(this.bt5_Click);
+            // 
+            // bt6
+            // 
+            resources.ApplyResources(this.bt6, "bt6");
+            this.bt6.Name = "bt6";
+            this.bt6.UseVisualStyleBackColor = true;
+            this.bt6.Click += new System.EventHandler(this.bt6_Click);
+            // 
+            // bt1
+            // 
+            resources.ApplyResources(this.bt1, "bt1");
+            this.bt1.Name = "bt1";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // bt2
+            // 
+            resources.ApplyResources(this.bt2, "bt2");
+            this.bt2.Name = "bt2";
+            this.bt2.UseVisualStyleBackColor = true;
+            this.bt2.Click += new System.EventHandler(this.bt2_Click);
+            // 
+            // bt3
+            // 
+            resources.ApplyResources(this.bt3, "bt3");
+            this.bt3.Name = "bt3";
+            this.bt3.UseVisualStyleBackColor = true;
+            this.bt3.Click += new System.EventHandler(this.bt3_Click);
+            // 
+            // bt0
+            // 
+            resources.ApplyResources(this.bt0, "bt0");
+            this.bt0.Name = "bt0";
+            this.bt0.UseVisualStyleBackColor = true;
+            this.bt0.Click += new System.EventHandler(this.bt0_Click);
+            // 
+            // btClear
+            // 
+            resources.ApplyResources(this.btClear, "btClear");
+            this.btClear.Name = "btClear";
+            this.btClear.UseVisualStyleBackColor = true;
+            this.btClear.Click += new System.EventHandler(this.btClear_Click);
+            // 
+            // btPlus
+            // 
+            resources.ApplyResources(this.btPlus, "btPlus");
+            this.btPlus.Name = "btPlus";
+            this.btPlus.UseVisualStyleBackColor = true;
+            this.btPlus.Click += new System.EventHandler(this.btPlus_Click);
+            // 
+            // btMinus
+            // 
+            resources.ApplyResources(this.btMinus, "btMinus");
+            this.btMinus.Name = "btMinus";
+            this.btMinus.UseVisualStyleBackColor = true;
+            this.btMinus.Click += new System.EventHandler(this.btMinus_Click);
+            // 
+            // btAns
+            // 
+            resources.ApplyResources(this.btAns, "btAns");
+            this.btAns.Name = "btAns";
+            this.btAns.UseVisualStyleBackColor = true;
+            this.btAns.Click += new System.EventHandler(this.btAns_Click);
+            // 
+            // btDiv
+            // 
+            resources.ApplyResources(this.btDiv, "btDiv");
+            this.btDiv.Name = "btDiv";
+            this.btDiv.UseVisualStyleBackColor = true;
+            this.btDiv.Click += new System.EventHandler(this.btDiv_Click);
+            // 
+            // btMulti
+            // 
+            resources.ApplyResources(this.btMulti, "btMulti");
+            this.btMulti.Name = "btMulti";
+            this.btMulti.UseVisualStyleBackColor = true;
+            this.btMulti.Click += new System.EventHandler(this.btMulti_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btMulti);
+            this.Controls.Add(this.btDiv);
+            this.Controls.Add(this.btAns);
+            this.Controls.Add(this.btMinus);
+            this.Controls.Add(this.btPlus);
+            this.Controls.Add(this.btClear);
+            this.Controls.Add(this.bt0);
+            this.Controls.Add(this.bt3);
+            this.Controls.Add(this.bt2);
+            this.Controls.Add(this.bt1);
+            this.Controls.Add(this.bt6);
+            this.Controls.Add(this.bt5);
+            this.Controls.Add(this.bt4);
+            this.Controls.Add(this.bt9);
+            this.Controls.Add(this.bt8);
+            this.Controls.Add(this.bt7);
+            this.Controls.Add(this.txtbox);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtbox;
+        private System.Windows.Forms.Button bt7;
+        private System.Windows.Forms.Button bt8;
+        private System.Windows.Forms.Button bt9;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.Button bt5;
+        private System.Windows.Forms.Button bt6;
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.Button bt0;
+        private System.Windows.Forms.Button btClear;
+        private System.Windows.Forms.Button btPlus;
+        private System.Windows.Forms.Button btMinus;
+        private System.Windows.Forms.Button btAns;
+        private System.Windows.Forms.Button btDiv;
+        private System.Windows.Forms.Button btMulti;
+    }
+}
+
---
+++ calculator (1)/calculator/calculator/Form1.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        Boolean babala = false;
+        String temp="";
+        String ch="";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt0_Click(object sender, EventArgs e)//按鍵0
+        {
+            if(babala == true)//代表目前顯示的是上一次計算剩下來的答案
+            {
+                txtbox.Text = "";
+                txtbox.Text += bt0.Text;
+                babala = false;
+            }
+            else
+            {
+                if (txtbox.Text != "")//前面要有數字才允許輸入
+                txtbox.Text += bt0.Text;
+            }
+
+        }
+
+        private void bt1_Click(object sender, EventArgs e)//按鍵1
+        {
+            if (babala == true)//代表目前顯示的是上一次計算剩下來的答案
+            {
+                txtbox.Text = "";
+                txtbox.Text += bt1.Text;
+                babala = false;
+            }
+            else { txtbox.Text += bt1.Text; }
+        }
+
+        private void bt2_Click(object sender, EventArgs e)//按鍵2
+        {
+            if (babala == true)//代表目前顯示的是上一次計算剩下來的答案
+            {
+                txtbox.Text = "";
+                txtbox.Text += bt2.Text;
+                babala = false;
+            }
+            else { txtbox.Text += bt2.Text; }
+        }
+
+        private void bt3_Click(object sender, EventArgs e)//按鍵3
+        {
+            if (babala == true)//代表目前顯示的是上一次計算剩下來的答案
+            {
+                txtbox.Text = "";
+                txtbox.Text += bt3.Text;
+                babala = false;
+            }
+            else { txtbox.Text += bt3.Text; }
+        }
+
+        private void bt4_Click(object sender, EventArgs e)//按鍵4
+        {
+            if (babala == true)//代表目前顯示的是上一次計算剩下來的答案
+            {
+                txtbox.Text = "";
+                txtbox.Text += bt4.Text;
+                babala = false;
+            }
+            else { txtbox.Text += bt4.Text; }
+        }
+
+        private void bt5_Click(object sender, EventArgs e)//按鍵5
+        {
+            if (babala == true)//代表目前顯示的是上一次計算剩下來的答案
+            {
+                txtbox.Text = "";
+                txtbox.Text += bt5.Text;
+                babala = false;
+            }
+            else { txtbox.Text += bt5.Text; }
+        }
+
+        private void bt6_Click(object sender, EventArgs e)//按鍵6
+        {
+            if (babala == true)//代表目前顯示的是上一次計算剩下來的答案
+            {
+                txtbox.Text = "";
+                txtbox.Text += bt6.Text;
+                babala = false;
+            }
+            else { txtbox.Text += bt6.Text; }
+        }
+
+        private void bt7_Click(object sender, EventArgs e)//按鍵7
+        {
+            if (babala == true)//代表目前顯示的是上一次計算剩下來的答案
+            {
+                txtbox.Text = "";
+                txtbox.Text += bt7.Text;
+                babala = false;
+            }
+            else { txtbox.Text += bt7.Text; }
+        }
+
+        private void bt8_Click(object sender, EventArgs e)//按鍵8
+        {
+            if (babala == true)//代表目前顯示的是上一次計算剩下來的答案
+            {
+                txtbox.Text = "";
+                txtbox.Text += bt8.Text;
+                babala = false;
+            }
+            else { txtbox.Text += bt8.Text; }
+        }
+
+        private void bt9_Click(object sender, EventArgs e)//按鍵9
+        {
+            if (babala == true)//代表目前顯示的是上一次計算剩下來的答案
+            {
+                txtbox.Text = "";
+                txtbox.Text += bt9.Text;
+                babala = false;
+            }
+            else { txtbox.Text += bt9.Text; }
+        }
+
+        private void btClear_Click(object sender, EventArgs e)//清除鍵
+        {
+            txtbox.Text = "";
+            temp = "";
+            ch = "";
+        }
+
+        private void btPlus_Click(object sender, EventArgs e)//加法鍵
+        {
+
+            if (babala == false || ch == "=")//沒有連續運算元或是前面是 =
+            {
+                if (ch=="")//代表前面沒有運算元或是剛算完=
+                {
+                temp = txtbox.Text;
+                ch = btPlus.Text;
+                }
+                else
+                {
+                double a, b,c;
+                a=Double.Parse(temp);
+                b = Double.Parse(txtbox.Text);
+                    switch (ch)
+                    {
+                    case "+":
+                        c = a + b;
+                        temp = c.ToString();
+                        break;
+                    case "-":
+                        c = a - b;
+                        temp = c.ToString();
+                        break;
+                    case "*":
+                        c = a * b;
+                        temp = c.ToString();
+                        break;
+                    case "/":
+                        c = a / b;
+                        temp = c.ToString();
+                        break;
+                    }
+                txtbox.Text = temp;
+                ch = btPlus.Text;
+                }
+                babala = true;
+            }
+         
+        }
+
+        private void btMinus_Click(object sender, EventArgs e)//減法鍵
+        {
+            if (babala == false || ch == "=")//沒有連續運算元或是前面是 =
+            {
+                if (ch == "")
+                {
+                temp = txtbox.Text;
+                ch = btMinus.Text;
+                }
+                else
+                {
+                double a, b, c;
+                a = Double.Parse(temp);
+                b = Double.Parse(txtbox.Text);
+                    switch (ch)
+                    {
+                    case "+":
+                        c = a + b;
+                        temp = c.ToString();
+                        break;
+                    case "-":
+                        c = a - b;
+                        temp = c.ToString();
+                        break;
+                    case "*":
+                        c = a * b;
+                        temp = c.ToString();
+                        break;
+                    case "/":
+                        c = a / b;
+                        temp = c.ToString();
+                        break;
+                    }
+                txtbox.Text = temp;
+                ch = btMinus.Text;
+                }
+                babala = true;
+            }
+                
+           
+        }
+
+        private void btMulti_Click(object sender, EventArgs e)//乘法鍵
+        {
+            if (babala == false || ch == "=")//沒有連續運算元或是前面是 =
+            {
+                if (ch == "")//代表前面沒有運算元或是剛算完=
+                {
+                    temp = txtbox.Text;
+                    ch = btMulti.Text;
+                }
+                else
+                {
+                    double a, b, c;
+                    a = Double.Parse(temp);
+                    b = Double.Parse(txtbox.Text);
+                    switch (ch)
+                    {
+                        case "+":
+                            c = a + b;
+                            temp = c.ToString();
+                            break;
+                        case "-":
+                            c = a - b;
+                            temp = c.ToString();
+                            break;
+                        case "*":
+                            c = a * b;
+                            temp = c.ToString();
+                            break;
+                        case "/":
+                            c = a / b;
+                            temp = c.ToString();
+                            break;
+                    }
+                    txtbox.Text = temp;
+                    ch = btMulti.Text;
+                }
+                babala = true;
+            }
+        }
+
+        private void btDiv_Click(object sender, EventArgs e)//除法鍵
+        {
+            if (babala == false || ch == "=")
+            {
+                if (ch == "")
+                {
+                temp = txtbox.Text;
+                ch = btDiv.Text;
+                }
+                else
+                {
+                double a, b, c;
+                a = Double.Parse(temp);
+                b = Double.Parse(txtbox.Text);
+                    switch (ch)
+                    {
+                    case "+":
+                        c = a + b;
+                        temp = c.ToString();
+                        break;
+                    case "-":
+                        c = a - b;
+                        temp = c.ToString();
+                        break;
+                    case "*":
+                        c = a * b;
+                        temp = c.ToString();
+                        break;
+                    case "/":
+                        c = a / b;
+                        temp = c.ToString();
+                        break;
+                    }
+                txtbox.Text = temp;
+                ch = btDiv.Text;
+                }
+            babala = true;
+            }
+            
+        }
+
+        private void btAns_Click(object sender, EventArgs e)//等於鍵
+        {
+            if (babala == false)
+            {
+                if (ch == "")
+                {
+                return;
+                 }
+                else//前面有計算
+                {
+                double a, b, c;
+                a = Double.Parse(temp);
+                b = Double.Parse(txtbox.Text);
+                    switch (ch)
+                    {
+                    case "+":
+                        c = a + b;
+                        temp = c.ToString();
+                        break;
+                    case "-":
+                        c = a - b;
+                        temp = c.ToString();
+                        break;
+                    case "*":
+                        c = a * b;
+                        temp = c.ToString();
+                        break;
+                    case "/":
+                        c = a / b;
+                        temp = c.ToString();
+                        break;
+                     }
+                txtbox.Text = temp;
+                ch = "=";
+                babala = true;
+                }
+            }
+            
+        }
+    }
+}
